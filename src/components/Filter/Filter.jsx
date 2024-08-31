8d1959812a4a64e2768264155a4742ce1a1cdb6b
zharuk.alex <zzharuk@gmail.com>
2024-08-31 16:03:24 +0300

    step-3

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,24 @@
+import { useDispatch, useSelector } from 'react-redux';
 import style from './Filter.module.css';
+import { setFilter } from 'reduxTodo/filterSlice';
+import { selectFilter } from 'reduxTodo/selectors';
 
 export const Filter = () => {
-  return <input className={style.input} placeholder="Find it" name="filter" />;
+  const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
+
+  const changeHandle = e => {
+    const { value } = e.target;
+    dispatch(setFilter(value));
+  };
+
+  return (
+    <input
+      className={style.input}
+      placeholder="Find it"
+      name="filter"
+      onChange={changeHandle}
+      value={filter}
+    />
+  );
 };
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,9 +1,29 @@
 import { Text } from 'components';
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import style from './Todo.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteToDo } from 'reduxTodo/toDoSlice';
 
 export const Todo = ({ id, text, index }) => {
+  const dispatch = useDispatch();
+
+  const removeTodo = () => {
+    dispatch(deleteToDo(id));
+  };
+
   return (
-    <Text textAlign="center" marginBottom="20">
-      TODO #{index}
-    </Text>
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        TODO # {index}
+      </Text>
+
+      <Text>{text}</Text>
+      <button className={style.deleteButton} type="button" onClick={removeTodo}>
+        <RiDeleteBinLine size={24} />
+      </button>
+      <button className={style.editButton} type="button">
+        <RiEdit2Line size={24} />
+      </button>
+    </div>
   );
 };
